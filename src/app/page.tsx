0118e0421c6fe1959f1dc50b2602768caae67eb3
devmustroc <elmustapha.abourar@outlook.com>
2025-06-26 16:25:17 +0200

    add TRPC setup with client and server integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+
+import {getQueryClient, trpc} from "@/trpc/server";
+
 export default function Home() {
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAi.queryOptions({ text: "Hello, Next.js!" }));
   return (
     <div>
       Hello, Next.js!

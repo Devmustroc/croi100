5ecef85790f4b2fc15cbff81818488494e2c0719
devmustroc <elmustapha.abourar@outlook.com>
2025-06-27 12:51:59 +0200

    add Inngest client setup and function integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,38 @@
+"use client";
 
-import {getQueryClient, trpc} from "@/trpc/server";
+import {Button} from "@/components/ui/button";
+import {useTRPC} from "@/trpc/client";
+import {useMutation} from "@tanstack/react-query";
+import {toast} from "sonner";
 
-export default function Home() {
-
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.createAi.queryOptions({ text: "Hello, Next.js!" }));
+const Page = () => {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: (data) => {
+      console.log("Function invoked successfully:", data);
+      toast.success("Function invoked successfully", {
+        description: `Response: ${JSON.stringify(data)}`,
+      });
+    },
+    onError: (error) => {
+      console.error("Error invoking function:", error);
+      toast.error("Error invoking function", {
+        description: `Error: ${error.message}`,
+      });
+    },
+  }))
   return (
-    <div>
-      Hello, Next.js!
+    <div
+      className="p-4 max-w-7xl mx-auto"
+    >
+      <Button
+        disabled={invoke.isPending}
+        onClick={() => invoke.mutate({ text: "world mustapha" })}
+      >
+        Invoke Inngest Function
+      </Button>
     </div>
   );
 }
+
+export default Page;
